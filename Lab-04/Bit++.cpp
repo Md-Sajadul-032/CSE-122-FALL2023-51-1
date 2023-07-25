918efcfdb03087a972e2d3d0e7988cb52a786480
Md. Sajadul Islam <139230778+Md-Sajadul-032@users.noreply.github.com>
2023-07-25 20:20:13 +0600

    Create  Bit++.cpp

---
+++ Lab-04/Bit++.cpp
@@ -0,0 +1,24 @@
+    #include <iostream>
+    #include <string>
+    using namespace std;
+     
+    int main()
+    {
+      int a, X = 0;
+      string s;
+      cin >> a;
+      for (int i = 0; i < a; i++)
+      {
+        cin >> s;
+        if (s[1] == '+')
+        {
+          X++;
+        }
+        else
+        {
+          X--;
+        }
+      }
+      cout << X << endl;
+      return 0;
+    }
