26bc68a8ccd02a5dbf1c6123d0a14d8956f8bf50
Md. Sajadul Islam <139230778+Md-Sajadul-032@users.noreply.github.com>
2023-07-25 20:18:24 +0600

    Create  Domino piling.cpp

---
+++ Lab-04/Domino piling.cpp
@@ -0,0 +1,11 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main()
+    {
+      int M , N , x;
+      cin >> M >> N;
+      x=M*N;
+      cout << x/2;
+      return 0;
+    }
