06071a888b67ec7207b035a2da7af2e9f3a604c9
Md. Sajadul Islam <139230778+Md-Sajadul-032@users.noreply.github.com>
2023-08-08 16:17:37 +0600

    Beautiful Matrix.cpp

--- Lab-05/Beautiful Matrix.cpp
+++ Lab-05/Beautiful Matrix.cpp
@@ -1,26 +1,15 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
  
-int main()
-{
+int main(){
   int x[5][5];
   for (int i = 0; i < 5; i++)
-  {
     for (int j = 0; j < 5; j++)
-    {
       cin >> x[i][j];
-    }
-  }
-  for (int i = 0; i < 5; i++)
-  {
+ 
+  for (int i = 0; i < 5; i++){
     for (int j = 0; j < 5; j++)
-    {
       if (x[i][j] == 1)
-      {
         cout << abs(i - 2) + abs(j - 2) << endl;
-      }
-    }
   }
-  return 0;
 }
