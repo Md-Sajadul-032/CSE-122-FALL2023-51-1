cddd9cfa7e2977baf9da85dc1f7286b8623d0bfd
Md. Sajadul Islam <139230778+Md-Sajadul-032@users.noreply.github.com>
2023-09-26 11:50:47 +0600

    Word Capitalization.cpp

---
+++ Lab-06/Word Capitalization.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+  string s;
+  cin >> s;
+  if(s[0]>=97){
+    s[0]=s[0]-32;
+  }else{
+    s[0]=s[0];
+  }
+  cout << s;
+  return 0;
+}
