b4f67f4653dc6600478889d327d0a83046b371bd
Md. Sajadul Islam <139230778+Md-Sajadul-032@users.noreply.github.com>
2023-10-03 09:09:07 +0600

    Create Elephant.cpp

---
+++ Lab-07/Elephant.cpp
@@ -0,0 +1,13 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main (){
+        int x , a;
+        cin >> x;
+        a=x/5;
+        if(x%5==0)
+        cout << a;
+        else
+        cout << a+1;
+        return 0;
+    }
